29e836ce0df5e57f42d4215985ceab6893aeb848
mr <rezakazemi890@gmail.com>
2023-02-28 15:06:09 +0330

    Added FibonaccianSearch Sample

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -45,6 +45,17 @@
                               + binarySearchRecursiveResult);
         #endregion
 
+        #region FibonaccianSearch
+
+        int FibonaccianSearchResult = FibonaccianSearch(arr, x, arr.Length);
+        if (FibonaccianSearchResult == -1)
+            Console.WriteLine(
+                "FibonaccianSearch O(Log n): Element is not present in array");
+        else
+            Console.WriteLine("FibonaccianSearch O(Log n): Element is present at index "
+                              + FibonaccianSearchResult);
+        #endregion
+
         Console.ReadLine();
     }
 
@@ -151,6 +162,66 @@
 
         // We reach here when element is not present
         // in array
+        return -1;
+    }
+
+    public static int FibonaccianSearch(int[] arr, int x, int length)
+    {
+        /* Initialize fibonacci numbers */
+        int fibMMm2 = 0; // (m-2)'th Fibonacci No.
+        int fibMMm1 = 1; // (m-1)'th Fibonacci No.
+        int fibM = fibMMm2 + fibMMm1; // m'th Fibonacci
+
+        /* fibM is going to store the smallest
+        Fibonacci Number greater than or equal to n */
+        while (fibM < length)
+        {
+            fibMMm2 = fibMMm1;
+            fibMMm1 = fibM;
+            fibM = fibMMm2 + fibMMm1;
+        }
+
+        // Marks the eliminated range from front
+        int offset = -1;
+
+        /* while there are elements to be inspected.
+        Note that we compare arr[fibMm2] with x.
+        When fibM becomes 1, fibMm2 becomes 0 */
+        while (fibM > 1)
+        {
+            // Check if fibMm2 is a valid location
+            int i = Math.Min(offset + fibMMm2, length - 1);
+
+            /* If x is greater than the value at
+            index fibMm2, cut the subarray array
+            from offset to i */
+            if (arr[i] < x)
+            {
+                fibM = fibMMm1;
+                fibMMm1 = fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+                offset = i;
+            }
+
+            /* If x is less than the value at index
+            fibMm2, cut the subarray after i+1 */
+            else if (arr[i] > x)
+            {
+                fibM = fibMMm2;
+                fibMMm1 = fibMMm1 - fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+            }
+
+            /* element found. return index */
+            else
+                return i;
+        }
+
+        /* comparing the last element with x */
+        if (fibMMm1 == 1 && arr[length - 1] == x)
+            return length - 1;
+
+        /*element not found. return -1 */
         return -1;
     }
 }
